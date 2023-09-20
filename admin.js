4ee34e8f2abf5d7be831b73bb66c9f927c666daf
prashant9191 <sonupv000@gmail.com>
2023-09-20 16:44:26 +0530

    deployed backend server and replaced links

---
+++ admin.js
@@ -0,0 +1,90 @@
+// Script for fetching and populating contact data
+document.addEventListener('DOMContentLoaded', () => {
+    const contactTable = document.querySelector('.main_admin_content_contact_table table tbody');
+  
+    // Function to create table rows for contact data
+    const createContactTableRow = (data, index) => {
+      const row = document.createElement('tr');
+      row.innerHTML = `
+        <td>${index + 1}</td>
+        <td>${data.finaldate}</td>
+        <td>${data.finaltime}</td>
+        <td>${data.name}</td>
+        <td>${data.phone}</td>
+        <td>${data.email}</td>
+        <td class="Contact_message_td">${data.message}</td>
+        <td>
+          <select class="Contact_us_Action" data-contact-id="${data._id}">
+            <option value="acnt">Action Not Taken</option>
+            <option value="act">Action Taken</option>
+          </select>
+        </td>
+      `;
+  
+      // Set the background color based on the status
+      if (data.status === "act") {
+        row.style.background = "#009688";
+      }
+  
+      return row;
+    };
+  
+    // Function to fetch contact data from an API
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://dark-pink-eagle-tux.cyclic.cloud/contact/"
+        );
+        const contactData = await response.json();
+  
+        // Add table rows for each contact entry
+        contactData.reverse().forEach((data, index) => {
+          contactTable.appendChild(createContactTableRow(data, index));
+        });
+      } catch (error) {
+        console.error("Error:", error);
+        // Handle error states if necessary
+      }
+    };
+  
+    // Initial data fetching
+    fetchData();
+  
+    // Function to handle status change
+    const handleStatusChange = async (event) => {
+      const newStatus = event.target.value;
+      const contactId = event.target.getAttribute("data-contact-id");
+  
+      try {
+        const response = await fetch(
+          `https://dark-pink-eagle-tux.cyclic.cloud/contact/editcontact/${contactId}`,
+          {
+            method: 'PATCH',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              status: newStatus,
+            }),
+          }
+        );
+  
+        if (response.status === 200) {
+          // Update the status in the table row
+          const row = event.target.parentElement.parentElement;
+          row.style.background = newStatus === "act" ? "#009688" : "";
+        }
+      } catch (error) {
+        console.error("Error:", error);
+        // Handle error states if necessary
+      }
+    };
+  
+    // Event delegation for status change
+    contactTable.addEventListener('change', (event) => {
+      if (event.target.classList.contains('Contact_us_Action')) {
+        handleStatusChange(event);
+      }
+    });
+  });
+  
---
+++ login.js
@@ -0,0 +1,47 @@
+// login.js
+
+// Get a reference to the form and its elements
+const loginForm = document.querySelector('form');
+const emailInput = document.querySelector('input[name="email"]');
+const passwordInput = document.querySelector('input[name="password"]');
+
+// Add a submit event listener to the form
+loginForm.addEventListener('submit', async (e) => {
+  e.preventDefault(); // Prevent the default form submission
+
+  // Get the user's input values
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
+  // Create an object with the login data
+  const loginData = {
+    email: email,
+    password: password,
+  };
+
+  try {
+    const response = await fetch("https://dark-pink-eagle-tux.cyclic.cloud/user/login", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(loginData),
+    });
+
+    if (response.status === 200) {
+      const data = await response.json();
+      const { token } = data;
+      sessionStorage.setItem("token", token);
+      sessionStorage.setItem("email", email);
+      // Redirect to the admin page or perform any other action here
+      window.location.href = "./index.html";
+    } else if (response.status === 201) {
+      const data = await response.json();
+      alert(data.msg);
+    } else {
+      alert("An error occurred while processing your request.");
+    }
+  } catch (error) {
+    alert(error.message);
+  }
+});
